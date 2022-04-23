7c21432b995bc8ea0761d8dc9e22dac3450b60cf
Mikhail Smirnov <urine89@mail.ru>
2022-04-24 02:12:43 +0300

    Delete unused import

--- server-nest/src/posts/dto/create-post.dto.ts
+++ server-nest/src/posts/dto/create-post.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, Length, IsUUID } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class CreatePostDto {
     @IsString({ message: 'Has to be string' })
